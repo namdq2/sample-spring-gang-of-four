45a021d5e89d89f5f7964545901635269c6c2e58
namquocdang <namquoc.dev@gmail.com>
2019-10-30 12:10:53 +0700

    Added equals and hashcode in Test class of the "Builder" pattern.

--- src/test/java/com/namdq/gof/builder/DirectorTest.java
+++ src/test/java/com/namdq/gof/builder/DirectorTest.java
@@ -2,21 +2,38 @@ package com.namdq.gof.builder;
 
 import com.namdq.gof.builder.impl.ConcreteBuilder1;
 import com.namdq.gof.builder.impl.ConcreteBuilder2;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DirectorTest {
 
     @Test
     public void constructProduct1() {
-        Builder builder = new ConcreteBuilder1();
+        ConcreteBuilder1 builder = new ConcreteBuilder1();
         Director director = new Director(builder);
         director.constructProduct();
+        Product actual = builder.getProduct();
+
+        Product expected = new Product();
+        expected.setProperty1("stepA");
+        expected.setProperty2("stepB");
+        expected.setProperty3("stepZ");
+
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void constructProduct2() {
-        Builder builder = new ConcreteBuilder2();
+        ConcreteBuilder2 builder = new ConcreteBuilder2();
         Director director = new Director(builder);
         director.constructProduct();
+        Product actual = builder.getProduct();
+
+        Product expected = new Product();
+        expected.setProperty1("stepA");
+        expected.setProperty2("stepB");
+        expected.setProperty3("stepZ");
+
+        Assertions.assertEquals(expected, actual);
     }
 }

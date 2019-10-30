da06d49564d5abb7316a762408b6294f121376fc
namquocdang <namquoc.dev@gmail.com>
2019-10-30 12:08:41 +0700

    added equals and hashcode method in Product class of Builder pattern

--- src/main/java/com/namdq/gof/builder/Product.java
+++ src/main/java/com/namdq/gof/builder/Product.java
@@ -1,10 +1,12 @@
 package com.namdq.gof.builder;
 
+import lombok.EqualsAndHashCode;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter
 @ToString
+@EqualsAndHashCode
 public class Product {
     private String property1;
     private String property2;
